70d3ba8f5df1482bd785b030d23195afde733df9
luizbezerra-pinheiro <luizjosebp@gmail.com>
2019-11-15 02:46:48 +0100

    [fix/feat] Add optimized queries and fix other queries

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,6 +1,6 @@
 -- 1st
 
-EXPLAIN ANALYSE
+EXPLAIN
 SELECT C.name, P0.name
 FROM country C, artist P0
 WHERE C.id = P0.area
@@ -29,12 +29,32 @@ FROM country C
 INNER JOIN artist A ON A.area = C.id
 WHERE C.name IN (SELECT name FROM max_countries);
 
--- 3th
+-- 3th --------------
 
 EXPLAIN ANALYSE
 SELECT DISTINCT P0.id
 FROM release R, release_country RC, country C, artist P0, release_has_artist RhA
 WHERE RhA.artist = P0.id AND RhA.release = R.id AND RC.release = R.id AND RC.country = C.id AND C.name LIKE 'A%';
+
+-- Optimized 3th
+EXPLAIN ANALYSE
+WITH ACountries AS
+    (
+     SELECT C.id
+     FROM country C
+     WHERE C.name LIKE 'A%'
+    ),
+     releasesAtA AS
+    (
+        SELECT RC.release
+        FROM release_country RC
+        INNER JOIN ACountries C ON RC.country = C.id
+    )
+    SELECT R.artist
+    FROM release_has_artist R
+    INNER JOIN releasesAtA RAA ON R.release = RAA.release
+    GROUP BY R.artist
+    ORDER BY R.artist;
 
 -- 4th
 
@@ -70,7 +90,7 @@ FROM release_country RC, release R
 WHERE RC.release = R.id AND ( (SELECT COUNT(*) FROM track T, release R1 WHERE T.release = R1.id) >= 2)
 ORDER BY (RC.release, RC.country);
 
--- 8th
+-- 8th -------
 
 EXPLAIN ANALYSE
 SELECT RC.release, RC.country, RC.year, RC.month, RC.day
@@ -83,35 +103,59 @@ WHERE RC.country <> (
                     )
 ORDER BY (RC.year, RC.month, RC.day, RC.country);
 
+-- Optimized 8th
+
+CREATE VIEW ReleaseCountryOrdered AS
+    (SELECT row_number() OVER (PARTITION BY RC.release ORDER BY  RC.year, RC.month, RC.day) release_order, *
+    FROM release_country RC);
+
+EXPLAIN ANALYSE
+SELECT RC.release, RC.country
+FROM ReleaseCountryOrdered RC
+WHERE RC.release_order > 1;
+
+
 -- 9th
+-- OLD QUERY
 
--- First a query to count the number of people born before a specific Person id = 228117
--- P must be born before P0
-
-EXPLAIN
 SELECT C.name, P0.name,
     (
         SELECT COUNT(*)
         FROM artist P
-        WHERE P.area = P0.area AND P.type = 1 AND
-              ( (P0.syear IS NULL OR P.syear IS NULL ) OR
+        WHERE P.area = P0.area AND
+              (
+		        (P.syear IS NOT NULL) AND
                 ( P0.syear > P.syear OR ( P0.syear = P.syear AND
-                ( (P0.smonth IS NULL OR P.smonth IS NULL) OR
+                ( (P0.smonth IS NOT NULL AND P.smonth IS NOT NULL) AND
                 ( P0.smonth > P.smonth OR (P0.smonth = P.smonth AND
-                ( (P0.sday IS NULL OR P.sday IS NULL)
-                OR (P0.sday >= P.sday))))))))
+                ( (P0.sday IS NOT NULL AND P.sday IS NOT NULL)
+                AND (P0.sday > P.sday))))))))
         ) as nb,
-       (
+
+    (
         SELECT COUNT(*)
         FROM artist P
-        WHERE   P.type = 1 AND ( (P0.syear IS NULL OR P.syear IS NULL ) OR
+        WHERE (
+		        ( P.syear IS NOT NULL) AND
                 ( P0.syear > P.syear OR ( P0.syear = P.syear AND
-                ( (P0.smonth IS NULL OR P.smonth IS NULL) OR
+                ( (P0.smonth IS NOT NULL AND P.smonth IS NOT NULL) AND
                 ( P0.smonth > P.smonth OR (P0.smonth = P.smonth AND
-                ( (P0.sday IS NULL OR P.sday IS NULL)
-                OR (P0.sday >= P.sday))))))))
-           ) as nb_global
-FROM artist P0, country C
-WHERE P0.type = 1 -- 1 -> 'Person'
-ORDER BY nb, nb_global
-LIMIT 2;
+                ( (P0.sday IS NOT NULL AND P.sday IS NOT NULL)
+                AND (P0.sday > P.sday))))))))
+        ) as nb_global
+FROM artist P0
+INNER JOIN country C ON C.id = P0.area
+WHERE P0.type = 1 AND P0.syear IS NOT NULL; -- 1 -> 'Person';
+
+-- Optimized 9th
+CREATE VIEW ArtistsOrdered AS
+    (SELECT row_number() OVER (ORDER BY A.syear, A.smonth, A.sday) - 1 AS nb_global, row_number() OVER (PARTITION BY area ORDER BY area, syear, smonth, sday) - 1 AS nb, *
+    FROM Artist A
+    WHERE A.syear IS NOT NULL AND A.smonth IS NOT NULL AND A.sday IS NOT NULL);
+
+EXPLAIN ANALYSE
+SELECT C.name as country, P0.name as name, P0.nb, P0.nb_global
+FROM ArtistsOrdered P0
+INNER JOIN country c on P0.area = c.id
+WHERE P0.type = 1
+ORDER BY nb, nb_global;
